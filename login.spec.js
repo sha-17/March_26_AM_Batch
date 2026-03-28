4fc94b2854c0062c606f9b7d0b46f0ce46a95ddc
sha-17 <shahussain.arif@gmail.com>
2026-03-28 11:58:28 +0530

    added

--- login.spec.js
+++ login.spec.js
@@ -1,6 +1,7 @@
 import {test,expect} from '@playwright/test';
 
-test {login ,async({page})=>{
+test("login", async ({page}) => {
 
   await page.goto("https://testautomationpractice.blogspot.com/")
-}
+  await page.click("#HTML9 > div.widget-content > button")
+});
